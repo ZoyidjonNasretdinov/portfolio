c45eb18f25c9baad8c9d6b9afe5cd5cc5de1b2ab
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-02-23 22:57:29 +0500

    Seo optimize

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,4 +1,5 @@
 import { Layout } from '@/components'
+import Projects from '@/components/sections/projects'
 import ServiceAbout from '@/components/sections/serviceAbout'
 import ServiceStatistics from '@/components/sections/serviceStats'
 import React from 'react'
@@ -6,16 +7,17 @@ import React from 'react'
 const Services = () => {
   return (
     <Layout>
-      <div className="container">
+      <div className="flex gap-3">
         <div className="flex flex-col w-[330px] gap-3">
           <ServiceStatistics />
           <ServiceAbout />
+          <Projects />
         </div>
         <div className="col"></div>
-        <div className="col"></div>
+        <div className="flex flex-col w-[270px] gap-3"></div>
       </div>
     </Layout>
   )
 }
 
-export default Services
+export default Services 
