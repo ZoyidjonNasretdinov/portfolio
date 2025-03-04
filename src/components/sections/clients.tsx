5acd2d8763a98ce14bb5faef4f8b157262e46b5c
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-03-04 22:08:21 +0500

    Client section mobile layout

--- src/components/sections/clients.tsx
+++ src/components/sections/clients.tsx
@@ -3,14 +3,14 @@ import { HiClipboardDocumentList } from "react-icons/hi2";
 
 const Clients = () => {
   return (
-    <section className="bg-primary rounded-[1vw] w-full overflow-hidden relative shadow-lg h-[30vh] tablet:h-[35vh] mobile:h-[40vh]">
+    <section className="bg-primary rounded-[1vw] mobile:rounded-[4vw] w-full overflow-hidden relative shadow-lg h-[30vh] tablet:h-[35vh] mobile:h-[40vh]">
       {/* Header */}
       <div className="px-[2vw] py-[2vh]">
-        <h2 className="flex items-center justify-center gap-[0.5vw] text-white mb-[1vh] text-[1.2vw] mobile:text-[3vw]">
-          <HiClipboardDocumentList className="text-textSecondary text-[1.5vw] mobile:text-[4vw]" />
+        <h2 className="flex items-center justify-center gap-[0.5vw] text-white mb-[1vh] text-[1.2vw] mobile:text-[4vw]">
+          <HiClipboardDocumentList className="text-textSecondary text-[1.5vw] mobile:text-[5vw]" />
           My Clients
         </h2>
-        <p className="text-[0.9vw] text-white flex justify-center mb-[2vh] mobile:text-[2.5vw]">
+        <p className="text-[0.9vw] text-white flex justify-center mb-[2vh] mobile:text-[5.5vw]">
           Satisfied Partners
         </p>
       </div>
@@ -23,7 +23,7 @@ const Clients = () => {
             {["Google", "Amazon", "Facebook", "Apple", "Microsoft"].map((client, index) => (
               <li
                 key={index}
-                className="flex-shrink-0 w-[10vw] h-[6vh] bg-white text-primary font-bold rounded-lg shadow-md flex items-center justify-center text-[1vw] mobile:text-[2.5vw]"
+                className="flex-shrink-0 w-[10vw] mobile:w-[25vw] mobile:h-[9vh] h-[6vh] bg-white text-primary font-bold rounded-lg shadow-md flex items-center justify-center text-[1vw] mobile:text-[4.5vw]"
               >
                 {client}
               </li>
@@ -37,7 +37,7 @@ const Clients = () => {
             {["Tesla", "Netflix", "Adobe", "Intel", "Samsung"].map((client, index) => (
               <li
                 key={index}
-                className="flex-shrink-0 w-[10vw] h-[6vh] bg-secondary text-white font-bold rounded-lg shadow-md flex items-center justify-center text-[1vw] mobile:text-[2.5vw]"
+                className="flex-shrink-0 w-[10vw] h-[6vh] mobile:w-[25vw] mobile:h-[9vh] bg-secondary text-white font-bold rounded-lg shadow-md flex items-center justify-center text-[1vw] mobile:text-[4.5vw]"
               >
                 {client}
               </li>
