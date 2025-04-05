1bdffdf4df657323810fead499b4ba9d210af917
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.con>
2025-04-05 11:23:52 +0500

    fix service card

--- src/components/sections/servicesCards.tsx
+++ src/components/sections/servicesCards.tsx
@@ -36,13 +36,13 @@ const ServicesCards = () => {
       {services.map((service, index) => (
         <div
           key={index}
-          className="w-[full] flex-1 bg-[#111] p-[1.5vw] rounded-2xl text-white shadow-md"
+          className="w-[full] flex-1 bg-[#111] p-[1.5vw] rounded-[1vw] text-white shadow-md"
         >
           <div className="flex items-center gap-[1vw] text-[1.2vw] font-bold">
-            <span className="p-2 bg-[#222] rounded-[0.5vw] ">{service.icon}</span>
+            <span className="p-[0.3vw] bg-[#222] rounded-[0.5vw] ">{service.icon}</span>
             {service.title}
           </div>
-          <p className="text-gray-400 mt-[0.8vw]">{service.description}</p>
+          <p className="text-gray-400 mt-[0.8vw] text-[1vw]">{service.description}</p>
 
           <div className="mt-4 space-y-[1vw] text-[0.9vw]">
             <div className="flex justify-between border-b border-gray-700 pb-[0.5vw]">
@@ -59,7 +59,7 @@ const ServicesCards = () => {
             </div>
           </div>
 
-          <button className="w-full flex items-center justify-center gap-[0.5vw] bg-[#222] mt-[0.8vw] py-[0.6vw] rounded-lg text-white hover:bg-[#333] transition">
+          <button className="w-full flex items-center justify-center gap-[0.5vw] bg-[#222] mt-[0.8vw] py-[0.6vw] rounded-[0.5vw] text-white hover:bg-[#333] transition text-[1vw]">
             <BsCalendarEvent className="text-purple-400" />
             Schedule Call
           </button>
