363fd254f7d16c02f0d08b2865326fac748f6cbb
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.con>
2025-04-05 11:57:01 +0500

    fix error verecel

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,13 +1,12 @@
 import { Layout } from '@/components'
 import React from 'react'
-import WorkStatistics from '@/components/sections/work-stats'
+
 
 const Works = () => {
   return (
     <Layout>
       <div className="flex gap-[1vw] p-[1vw]">
         <div className='w-[20vw]'>
-          <WorkStatistics />
         </div>
         <div className='w-[56vw]'></div>
         <div className='w-[20vw]'></div>
